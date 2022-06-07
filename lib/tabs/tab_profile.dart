ae4b26eb4139b939544cbdbb0a7f99f056f89389
Luke Lee <hslee1@seegene.com>
2022-06-07 15:59:34 +0900

    Add logout function

--- lib/tabs/tab_profile.dart
+++ lib/tabs/tab_profile.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:simple_shopping/models/model_auth.dart';
+import 'package:provider/provider.dart';
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({Key? key}) : super(key: key);
 
   @override
+  createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Profile'));
+    final authClient =
+        Provider.of<FirebaseAuthProvider>(context, listen: false);
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Profile'),
+        TextButton(
+            onPressed: () async {
+              await authClient.logout();
+
+              if (!mounted) return;
+              ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(
+                    const SnackBar(content: Text('Success to logout')));
+              Navigator.of(context).pushReplacementNamed('/login');
+            },
+            child: const Text('Logout'))
+      ],
+    ));
   }
 }
